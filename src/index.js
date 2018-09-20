a69fe46c6ea0be6e4178af5ea524b26796e298b7
dmsolov <43238296+dmsolov@users.noreply.github.com>
2018-09-20 23:32:39 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,4 +4,18 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  var k=0, a, b, c;
+  var arr = [];
+  for(var i = 0; i<preferences.length; i++)
+  {
+     a = preferences[i];
+     b = preferences[a-1];
+     c = preferences[b-1];
+     if (i == (c-1) && arr.indexOf(a) === -1 && b !=c)
+     {
+        k++;
+        arr.push(a, b, c);
+     }  
+  }
+  return k;
 };
